2597c8ddb3e6674da5cec10f464332c91b109bbd
m_maohuawei <maohuawei@bdlbsc.com>
2020-09-16 17:14:42 +0800

    init

---
+++ sdk/src/main/java/com/ms/module/impl/login3/alipay/AliPayImpl.java
@@ -0,0 +1,17 @@
+package com.ms.module.impl.login3.alipay;
+
+import android.app.Activity;
+
+import com.ms.module.supers.inter.login3.IAliPayLoginAdapter;
+import com.ms.module.supers.inter.login3.LoginCallBack;
+
+public class AliPayImpl extends IAliPayLoginAdapter {
+
+    @Override
+    public void login(Activity activity, LoginCallBack callBack) {
+        super.login(activity, callBack);
+
+
+
+    }
+}
